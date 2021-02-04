aa91f350bd68c1aad8759b27e7afc1388d322015
vishnugopy <vishnugoby0010@gmail.com>
2021-02-04 15:19:24 +0100

    JeSuisPro

--- first.js
+++
@@ -1,9 +0,0 @@
-function ageCheck(age){
-    if (age >= 18){
-        return "Vous pouvez entrer  car vous avez " + age + " ans";
-    }
-        return "Site interdit aux mineurs.";
-    }
-
-console.log(ageCheck(10));
-console.log(ageCheck(19));
